27269dc147933339e62f1b5524ee58158c2c9504
Andrew Costa Silva <andrew.costa@take.net>
2023-10-18 16:42:28 -0300

    feat: added task by id query

--- schema.js
+++ schema.js
@@ -4,8 +4,11 @@ const typeDefs = readFileSync('./schema.graphql', { encoding: 'utf-8' });
 
 const resolvers = {
   Query: {
-    getTasks() {
+    tasks() {
       return tasks
+    },
+    task(_root, args) {
+      return tasks.find(task => task.id == args.id)
     }
   }
 }
@@ -15,6 +18,11 @@ const tasks = [
     id: "1",
     title: "Hello world",
     description: "Task description"
+  },
+  {
+    id: "2",
+    title: "Hello world 2",
+    description: "Task description 2"
   }
 ]
 
