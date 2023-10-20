9280a4803adf2bee23cc7f51c383080ffce8da6b
Andrew Costa Silva <andrew.costa@take.net>
2023-10-20 13:31:27 -0300

    feat: changed to airbnb base lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['eslint:recommended', 'airbnb'],
+  extends: ['eslint:recommended', 'airbnb-base/legacy'],
   overrides: [
     {
       env: {
@@ -23,4 +23,4 @@ module.exports = {
   },
   rules: {
   }
-}
+};
